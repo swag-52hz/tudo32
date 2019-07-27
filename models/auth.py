fd236368b15082bc6268130bff38bed5b657eed8
swag <1254846545@qq.com>
2019-07-27 12:56:00 +0800

    添加users表

--- models/auth.py
+++ models/auth.py
@@ -4,7 +4,7 @@ from models.db import Base
 
 
 class User(Base):
-    __tablename__ = 'user'
+    __tablename__ = 'users'
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(50), unique=True, nullable=False)
     password = Column(String(50))
